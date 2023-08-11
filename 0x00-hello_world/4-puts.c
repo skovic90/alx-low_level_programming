d9798bd7d1e9f869ae66253637ed4c62b70c4670
skovic90 <skovic90@gmail.com>
2023-08-11 08:43:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts fuction
+ * Return: 0 (Passed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
