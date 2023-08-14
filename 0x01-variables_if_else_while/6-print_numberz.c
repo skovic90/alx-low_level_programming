27c85d1205177c35932144be324daa57f5c43a85
skovic90 <skovic90@gmail.com>
2023-08-14 08:48:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+putchar('0');
+putchar('1');
+putchar('2');
+putchar('3');
+putchar('4');
+putchar('5');
+putchar('6');
+putchar('7');
+putchar('8');
+putchar('9');
+putchar('\n');
+
+return (0);
+}
