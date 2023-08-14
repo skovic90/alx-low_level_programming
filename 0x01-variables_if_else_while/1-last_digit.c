b70840f8096bf307de376430d2b98f7ec822cbcc
skovic90 <skovic90@gmail.com>
2023-08-14 06:51:53 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	if (m > 5)
 		printf("last digit of %d is %d and is grater than 5", n, m);
 	else if (m == 0)
-		printf("last digit of %id is %d and is zero", n, m);
+		printf("last digit of %id is %d and is 0", n, m);
 	else if (m < 6 && m != 0)
 		printf("last digit of %d is %d and is less than 6 not 0", n, m);
 	printf("\n");
