584e46cb825a7b627029cd7096298defbc5536cc
skovic90 <skovic90@gmail.com>
2023-08-18 20:01:32 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,25 +9,25 @@
 
 void print_triangle(int size)
 {
-	int row, hashes, spaces;
+	int x, y, z;
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (row = 1; row <= size; row++)
-		{
-			for (spaces = size - row; spaces >= 1; spaces--)
-			{
-				_putchar(' '0;
-			}
-			for (hashes = 1; hashes <= row; hashes++)
-			{
-			        _putchar('#');
-			}
-			_putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
