9ff87145e838b7cb7c7f53ee0bb9746dd1dc38e1
skovic90 <skovic90@gmail.com>
2023-09-15 17:18:24 -0700

    0-sum_them_all.c

--- 0-sum_them_all.c
+++ 0-sum_them_all.c
@@ -6,22 +6,22 @@
  * @n: The number of parameters passed to the function.
  * @..: A variable number of parameters to calculate the sum of.
  *
- * Return:  the sum of all parameters.
+ *
+ * Return: if n == 0 -0.
+ *Otherwise -  sum of all parameters.
  */
 
-int sum_them_all(const unsigned int n, ...) {
-    if (n == 0) {
-        return 0;
-    }
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
 
-    int sum = 0;
-    va_list ap; // Declare va_list here
-    va_start(ap, n); // Use va_list declared above
+	va_start(ap, n);
 
-    if (unsigned int i = 0; i < n; i++) {
-        sum += va_arg(ap, int); // Use ap for va_arg
-    }
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-    va_end(ap);
+	va_end(ap);
 
-    return (sum);
+	return (sum);
+}
